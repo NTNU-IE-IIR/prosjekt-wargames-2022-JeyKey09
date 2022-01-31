7bfd68f23f25c4c3602ec03e6656b70c4286b28b
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2022-01-31 11:34:57 +0100

    Start commit

---
+++ src/main/java/no/ntnu/mathijoh/wargame/App.java
@@ -0,0 +1,13 @@
+package no.ntnu.mathijoh.wargame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
