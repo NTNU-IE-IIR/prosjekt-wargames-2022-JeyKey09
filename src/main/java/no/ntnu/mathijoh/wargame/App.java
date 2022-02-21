29394aa00c912e64c2f86cc56ce314dfe22aad3f
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2022-02-21 14:23:24 +0100

    Deleted test code from main

--- src/main/java/no/ntnu/mathijoh/wargame/App.java
+++ src/main/java/no/ntnu/mathijoh/wargame/App.java
@@ -1,38 +1,10 @@
 package no.ntnu.mathijoh.wargame;
 
-import no.ntnu.mathijoh.wargame.units.Army;
-import no.ntnu.mathijoh.wargame.units.InfantryUnit;
-import no.ntnu.mathijoh.wargame.units.Unit;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-      Army testArmy = new Army("Test");
-      Army testArmy2 = new Army("Test");
-      if (testArmy.equals(testArmy2)) {
-        System.out.println("It works");
-      }
-      Unit testUnit = new InfantryUnit("name", 10);
-      testArmy.add(testUnit);
-      testArmy2.add(testUnit);
-      if (testArmy.equals(testArmy2)) {
-        System.out.println("This also works");
-      }
-
-      Unit testUnit01 = new InfantryUnit("name", 10);
-      Unit testUnit02 = new InfantryUnit("name", 10);
-      testArmy.add(testUnit01);
-      testArmy2.add(testUnit02);
-      if (testArmy.equals(testArmy2)) {
-        System.out.println("This also also works");
-      }
-      if (testUnit01.equals(testUnit02)){
-        System.out.println("These are the same");
-      }
+      //TODO: God help me
     }
 }
